2dd04a7a705eda33e2d271b6648fb4222aecb111
Sedat <sedat@Sedats-MacBook-Air.local>
2021-01-30 21:38:23 +0300

    first commit

---
+++ src/main/java/com/sdtbsp/demo/model/Person.java
@@ -0,0 +1,31 @@
+package com.sdtbsp.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.springframework.lang.NonNull;
+
+import javax.validation.constraints.NotBlank;
+import java.util.UUID;
+
+public class Person {
+
+    private final UUID id;
+
+
+    @NotBlank(message = "Bos olamaz.")
+    private final String name;
+
+
+    public Person(@JsonProperty("id") UUID id,
+                  @JsonProperty("name") String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
